55825c7dea4c9cc8192ea18cf7e9ac6bad245cca
Muheeb Bashir <your_email@example.com>
2026-01-16 16:56:13 +0530

    feat: update frontend dependencies and implement routing
    - Added new dependencies: react-hot-toast, react-router, autoprefixer, daisyui, postcss, and tailwindcss.
    - Removed App.css and react.svg files.
    - Refactored App.jsx to implement routing with new pages: HomePage, SignUpPage, LoginPage, NotificationsPage, CallPage, ChatPage, and OnboardingPage.
    - Created new page components for each route.
    - Updated main.jsx to wrap App component with BrowserRouter for routing support.
    - Added Tailwind CSS configuration and imported styles in index.css.

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,27 @@
-import React from 'react'
+import { Route, Routes } from "react-router";
+import HomePage from "./pages/HomePage.jsx";
+import SignUpPage from "./pages/SignUpPage.jsx";
+import LoginPage from "./pages/LoginPage.jsx";
+import NotificationsPage from "./pages/NotificationsPage.jsx";
+import CallPage from "./pages/CallPage.jsx";
+import ChatPage from "./pages/ChatPage.jsx";
+import OnboardingPage from "./pages/OnboardingPage.jsx";
+import { Toaster } from "react-hot-toast";
 
 export default function App() {
   return (
-    <div>App</div>
-  )
+    <div className="h-screen" data-theme="forest">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/register" element={<SignUpPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/notifications" element={<NotificationsPage />} />
+        <Route path="/call" element={<CallPage />} />
+        <Route path="/chat" element={<ChatPage />} />
+        <Route path="/onboarding" element={<OnboardingPage />} />
+      </Routes>
+
+      <Toaster/>
+    </div>
+  );
 }
